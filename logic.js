d8e63e73635080a6a6b9daacca8f28894d6771b0
Kelvin <kelvin0898@live.com>
2021-12-01 23:14:51 +1100

    showing the clicked number on display

--- logic.js
+++ logic.js
@@ -1,3 +1,38 @@
+const display = document.querySelector('.display-screen');
+const clear = document.querySelector('.clear');
+const percent = document.querySelector('.percent');
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const multiplyButton = document.querySelector('.multiply');
+const divideButton = document.querySelector('.divide');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+const zero = document.querySelector('.zero');
+const decimal = document.querySelector('.decimal');
+const equal = document.querySelector('.equal');
+const number = document.querySelectorAll('.number')
+const clearButton = document.querySelector('.clear')
+
+
+
+displayValue = '';
+
+number.forEach((num) => {
+  num.addEventListener('click', (e) => {
+    display.innerHTML = e.target.innerHTML
+  })
+});
+
+
+
+
 function sum(a,b) {
   return a+b;
 }
@@ -27,3 +62,4 @@ function operate(operator, x, y) {
     throw 'NaN'
   }
 }
+
